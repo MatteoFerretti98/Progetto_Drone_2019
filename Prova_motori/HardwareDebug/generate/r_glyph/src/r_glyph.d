ffda0f148cdecebf3bc115feda1b2d37212047d1
MatteoFerretti98 <49650379+MatteoFerretti98@users.noreply.github.com>
2019-12-08 16:01:44 +0100

    Parte3 - 4 motori

--- Prova_motori/HardwareDebug/generate/r_glyph/src/r_glyph.d
+++ Prova_motori/HardwareDebug/generate/r_glyph/src/r_glyph.d
@@ -7,27 +7,27 @@ generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: ../generate/r_g
 ../generate/r_glyph/src/Glyph\Config.h:
 generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: ../generate/r_glyph/src/Glyph\Preamble.h
 ../generate/r_glyph/src/Glyph\Preamble.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\platform.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\platform.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\./board/rdkrx63n/r_bsp.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\./board/rdkrx63n/r_bsp.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\mcu\rx63n\iodefine.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\mcu\rx63n\iodefine.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\yrdkrx63n.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\yrdkrx63n.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\mcu_info.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\mcu_info.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\hwsetup.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\hwsetup.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\lcd.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\lcd.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\sbrk.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_bsp\.\board\rdkrx63n\sbrk.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_rspi_rx600\src\r_rspi_rx600.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_rspi_rx600\src\r_rspi_rx600.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_rspi_rx600\r_rspi_rx600_config.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_rspi_rx600\r_rspi_rx600_config.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\platform.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\platform.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\./board/rdkrx63n/r_bsp.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\./board/rdkrx63n/r_bsp.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\mcu\rx63n\iodefine.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\mcu\rx63n\iodefine.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\yrdkrx63n.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\yrdkrx63n.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\mcu_info.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\mcu_info.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\hwsetup.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\hwsetup.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\lcd.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\lcd.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\sbrk.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_bsp\.\board\rdkrx63n\sbrk.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_rspi_rx600\src\r_rspi_rx600.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_rspi_rx600\src\r_rspi_rx600.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_rspi_rx600\r_rspi_rx600_config.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_rspi_rx600\r_rspi_rx600_config.h:
 generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: ../generate/r_glyph/src/r_glyph.h
 ../generate/r_glyph/src/r_glyph.h:
-generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_glyph\r_glyph_config.h
-C:\Users\Matteo\e2_studio\workspace\Prova_motori\generate\r_glyph\r_glyph_config.h:
+generate/r_glyph/src/r_glyph.obj generate/r_glyph/src/r_glyph.d: C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_glyph\r_glyph_config.h
+C:\Users\Matteo\Documents\GitHub\Progetto_Drone_2019\Prova_motori\generate\r_glyph\r_glyph_config.h:
